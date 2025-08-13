0c28cfcf6f5acca2dc5cb7612e336f370cbf3588
Stas <stas.r.d87@gmail.com>
2025-08-14 00:37:29 +1000

    -

--- src/components/home/CrashCount.tsx
+++ src/components/home/CrashCount.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components"
+import bg from '../../assets/Rectangle 31.svg';
 
 const StyledWrapper = styled.div`
-    width: 90%;
-    height:100px;
-    background-image: url('../../assets/Rectangle 31.svg')
-`
+  width: 90%;
+  height: 100px;
+  background-image: url(${bg});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 export default function CrashCount() {
   return (
     <StyledWrapper>CrashCount</StyledWrapper>
