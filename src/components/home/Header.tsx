ad81f6d2a8ea08fdd09c5af42626cf20b948d064
Stas <stas.r.d87@gmail.com>
2025-08-13 18:49:04 +1000

    -

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,23 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import gift from "../../assets/icons/gift.svg";
 import rules from "../../assets/icons/rules.svg";
+import avatar from "../../assets/avatar.jpg"
 const StyledWrapper = styled.div`
   display: flex;
 `;
 
 const StyledInfo = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 23px;
 `;
 
 const StyledUser = styled.div``;
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+border: none;
+background: transparent;
+&:nth-child(2) {
+    margin-left: 34px;
+  }
+`;
+
 
 const StyledIcon = styled.img``;
 
-const StyledUserImg = styled.image``;
+const StyledUserImg = styled.img``;
 
-const StyledUserTextWrapper = styled.div``;
+const StyledUserTextWrapper = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:right`;
 const StyledUserText = styled.span``;
 export default function Header() {
   return (
@@ -36,6 +50,7 @@ export default function Header() {
           <StyledUserText>Good Evening,</StyledUserText>
           <StyledUserText>Alex!</StyledUserText>
         </StyledUserTextWrapper>
+        <StyledUserImg src={avatar}/>
       </StyledUser>
     </StyledWrapper>
   );
