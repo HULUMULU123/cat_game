c37498cdf8d8abd1b94c1332a886695ff4f52925
Stas <stas.r.d87@gmail.com>
2025-08-15 15:41:38 +1000

    -

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Tasks from "./pages/Tasks";
+import Quiz from "./pages/Quiz";
+import Simulation from "./pages/Simulation";
+import Prize from "./pages/Prize";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="tasks/" element={<Tasks/>}/>
+          <Route path="quiz/" element={<Quiz/>}/>
+          <Route path="simulation/" element={<Simulation/>} />
+          <Route path="prize/" element={<Prize/>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Prize.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Prize() {
+  return (
+    <div>Prize</div>
+  )
+}
