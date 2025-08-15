83b87ab8f6c40e17338111b0f2ed7e68f55caf79
Stas <stas.r.d87@gmail.com>
2025-08-15 13:25:43 +1000

    -

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/home/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
+import Tasks from "./pages/Tasks";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/* Layout-обёртка для всех вложенных маршрутов */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="tasks/" element={<Tasks/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
