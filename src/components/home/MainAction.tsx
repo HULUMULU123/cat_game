e5c79d7d8d1242c1124ec2080f85629f4c7e14df
Stas <stas.r.d87@gmail.com>
2025-08-15 12:53:47 +1000

    -

---
+++ src/components/home/MainAction.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+import gift from '../../assets/icons/gift.svg'
+
+const StyledActionWrapper = styled.div`
+position: absolute;
+left: 50%;
+transform: translateY(-50%);
+bottom:50px;
+padding: 7px 40px;
+background: #fff;
+box-shadow: 1px 3px 6px 0px rgba(0, 223, 152, 0.19) inset;
+  -webkit-box-shadow: 1px 3px 6px 0px rgba(0, 223, 152, 0.19) inset;
+  -moz-box-shadow: 1px 3px 6px 0px rgba(0, 223, 152, 0.19) inset;
+  display:flex;
+
+`
+
+const StyledActionContentWrapper = styled.div`
+    width:100%;
+    height: 100%;
+    position: relative;
+`
+
+const StyledActionTextWrapper = styled.div`
+margin: 0 auto;`
+
+const StyledActionName = styled.span`
+color: #03624C;
+font-size: 12px;`
+
+const StyledActionTimer = styled.span`
+color: #03624C;
+font-size: 28px;`
+
+const StyledGiftWrapper = styled.div`
+position: absolute;
+top: -20px;
+right: -20px;
+background: #2CC295;
+
+`
+
+const StyledGiftImg = styled.img`
+width: 60%;`
+export default function MainAction() {
+  return (
+    <StyledActionWrapper>
+        <StyledActionContentWrapper>
+            <StyledActionTextWrapper>
+                <StyledActionName>СБОЙ НАЧАЛСЯ :</StyledActionName>
+                <StyledActionTimer>03 : 59 : 58</StyledActionTimer>
+            </StyledActionTextWrapper>
+            <StyledGiftWrapper>
+                <StyledGiftImg src={gift}/>
+            </StyledGiftWrapper>
+        </StyledActionContentWrapper>
+    </StyledActionWrapper>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from "../components/home/Header";
 import Model from "../components/home/Model";
 import CrashCount from "../components/home/CrashCount";
 import AdevertSection from "../components/home/AdevertSection";
+import MainAction from "../components/home/MainAction";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <CrashCount/>
       <AdevertSection />
+      <MainAction/>
     </Model>
   );
 }
