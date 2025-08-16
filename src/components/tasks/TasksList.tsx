1334b5a8dca0ca72fa5fecf782f8fad713c80347
Stas <stas.r.d87@gmail.com>
2025-08-16 19:49:20 +1000

    -

--- src/components/tasks/TasksList.tsx
+++ src/components/tasks/TasksList.tsx
@@ -3,17 +3,40 @@ import TaskItem from "./TaskItem"
 import advert from '../../assets/icons/advert.svg'
 const StyledContentWrapper = styled.div`
 margin: 0 auto;
-width: 95%;`
+width: 95%;
+
+overflow-y: scroll;   /* только вертикальный скролл */
+overflow-x: hidden;   /* горизонтального нет */
+padding-right: 5px;   /* отступ от контента */
+box-sizing: content-box;
+
+scrollbar-width: thin;
+  scrollbar-color: #E1FFFB #2CC2A9; /* активная | неактивная *//* чтобы padding не "съел" ширину */
+&::-webkit-scrollbar{
+   width: 10px; 
+}
+&::-webkit-scrollbar-track{
+  background: #2CC2A9;  /* неактивная часть */
+  border-radius: 8px;
+}
+
+&::-webkit-scrollbar-thumb{
+  background: #E1FFFB;  /* активная часть */
+  border-radius: 8px;
+}
+`
 
 const StyledTasksList = styled.ul`
 padding: 0;
 `
-
 export default function TasksList() {
   return (
     <StyledContentWrapper>
         <StyledTasksList>
-            <TaskItem name='test' img={advert} />
+            <TaskItem name='Просмотр рекламного ролика' img={advert} />
+            <TaskItem name='Просмотр рекламного ролика' img={advert} />
+            <TaskItem name='Просмотр рекламного ролика' img={advert} />
+            <TaskItem name='Просмотр рекламного ролика' img={advert} />
         </StyledTasksList>
     </StyledContentWrapper>
   )
