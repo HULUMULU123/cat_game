c5b7a79e8b1211c30f44292ad6bd558fcf63f359
Stas <stas.r.d87@gmail.com>
2025-08-18 00:34:52 +1000

    -

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import CoinCount from '../components/common/CoinCount'
 import SectionInfo from '../components/common/SectionInfo'
+import SectionContent from '../components/simulation/SectionContent'
 
 const StyledWrapper = styled.div`
     height:100vh;
@@ -13,7 +14,7 @@ export default function Simulation() {
     <StyledWrapper>
         <CoinCount/>
         <SectionInfo InfoName={'СИМУЛЯЦИЯ'} InfoExtra={'0 / 260'}/>
-            
+        <SectionContent />
     </StyledWrapper>
   )
 }
