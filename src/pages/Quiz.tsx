3888e861c0eef38de2ebd7a9588237590d8cea04
Stas <stas.r.d87@gmail.com>
2025-08-18 15:27:25 +1000

    -

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import CoinCount from '../components/common/CoinCount'
+import SectionInfo from '../components/common/SectionInfo'
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`
+    height:100vh;
+    width:100%;
+    backdrop-filter: blur(40px);
+`
 
 export default function Quiz() {
   return (
-    <div>Quiz</div>
+    <StyledWrapper>
+      <CoinCount/>
+      <SectionInfo InfoName={'НЕЙРОФИЛЬТР'} InfoExtra={'4 РАУНД'}/>
+    </StyledWrapper>
   )
 }
