6443ac059fef272170f1574f5bb6e89d714c2be1
Stas <stas.r.d87@gmail.com>
2025-08-18 22:46:25 +1000

    prize

--- src/pages/Prize.tsx
+++ src/pages/Prize.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import SectionInfo from '../components/common/SectionInfo'
+import CoinCount from '../components/common/CoinCount'
+const StyledWrapper = styled.div`
+    height:100vh;
+    width:100%;
+    backdrop-filter: blur(40px);
+`
 export default function Prize() {
   return (
-    <div>Prize</div>
+    <StyledWrapper>
+          <CoinCount/>
+          <SectionInfo InfoName={'ТУРНИРНАЯ ТАБЛИЦА'} InfoExtra={'25 / 07 / 25'}/>
+        </StyledWrapper>
   )
 }
