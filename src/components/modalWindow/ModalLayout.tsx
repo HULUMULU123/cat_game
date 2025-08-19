285471aee3221b2e7884d9b32d4f3a5863a4e321
Stas <stas.r.d87@gmail.com>
2025-08-19 17:49:03 +1000

    -

--- src/components/modalWindow/ModalLayout.tsx
+++ src/components/modalWindow/ModalLayout.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledModalLayout = styled.div`
+top: 0;
+left: 0;
 position: fixed;
 width: 100vw;
 height:100vh;
-filter: blur(20px);
+backdrop-filter: blur(10px);
 background: rgba(0,0,0,0.3);
 display: flex;
 `
