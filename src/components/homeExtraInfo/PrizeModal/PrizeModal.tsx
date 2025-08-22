2f5959a78a1ab0921c56d282c89d68114b440cee
Stas <stas.r.d87@gmail.com>
2025-08-22 12:19:54 +1000

    -

--- src/components/homeExtraInfo/PrizeModal/PrizeModal.tsx
+++ src/components/homeExtraInfo/PrizeModal/PrizeModal.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Header from '../common/Header'
 import SectionInfo from '../../common/SectionInfo'
 import PirzeCard from './PrizeCard'
+import PrizeDescription from './PrizeDescription'
 
 const StyledWrapper = styled.div`
   width:100%;
@@ -13,6 +14,7 @@ export default function PrizeModal({handleClose}) {
         <Header infoType='prize' handleClose={handleClose}/>
         <SectionInfo InfoName={'НАГРАДЫ ТЕКУЩЕГО СБОЯ'}/>
         <PirzeCard/>
+        <PrizeDescription/>
     </StyledWrapper>
   )
 }
