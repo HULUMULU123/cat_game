701da4f73ac76657614ac8c6f4cbb9037c8819c9
Stas <stas.r.d87@gmail.com>
2025-08-23 12:47:22 +0700

    -

--- src/components/homeExtraInfo/PrizeModal/PrizeModal.tsx
+++ src/components/homeExtraInfo/PrizeModal/PrizeModal.tsx
@@ -7,26 +7,7 @@ import PrizeDescription from './PrizeDescription'
 
 const StyledWrapper = styled.div`
   width:100%;
-  overflow-y: scroll;   /* только вертикальный скролл */
-  overflow-x: hidden;   /* горизонтального нет */
-    /* отступ от контента */
-  box-sizing: content-box;
 
-  scrollbar-width: thin;
-    scrollbar-color: #E1FFFB #2CC2A9; /* активная | неактивная *//* чтобы padding не "съел" ширину */
-  height: 100vh;
-  &::-webkit-scrollbar{
-    width: 4px; 
-  }
-  &::-webkit-scrollbar-track{
-    background: #2CC2A9;  /* неактивная часть */
-    border-radius: 10px;
-  }
-
-  &::-webkit-scrollbar-thumb{
-    background: #E1FFFB;  /* активная часть */
-    border-radius: 20px;
-  }
 `
 export default function PrizeModal({handleClose}) {
   return (
