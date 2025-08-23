b0d9a621e55b85a984b6d5414473bab0e5d0c412
Stas <stas.r.d87@gmail.com>
2025-08-23 15:05:02 +0700

    -

--- src/components/homeExtraInfo/Rules/RulesItem.tsx
+++ src/components/homeExtraInfo/Rules/RulesItem.tsx
@@ -5,11 +5,16 @@ const StyledRulesItem = styled.li`
 display: flex;
 flex-direction: column;
 align-items: center;
-justify-content: center;`
+justify-content: center;
+`
 
 const StyledRuleImg = styled.img``
 
-const StyledRuleText = styled.span``
+const StyledRuleText = styled.span`
+font-family: 'Roboto', sans-serif;
+font-size: 11px;
+text-align: center;
+color: var(--color-white-text);`
 
 export default function RulesItem({icon, text}) {
   return (
