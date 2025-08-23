c7ff77406d71366e213f033c4902f44421d255ab
Stas <stas.r.d87@gmail.com>
2025-08-23 14:05:51 +0700

    -

--- src/components/homeExtraInfo/Rules/RulesHeader.tsx
+++ src/components/homeExtraInfo/Rules/RulesHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import rule from '../../../assets/icons/rules.svg'
+import rules from '../../../assets/icons/rules.svg'
 import HeaderCloseBtn from '../common/HeaderCloseBtn'
 
 const StyledHeader = styled.div`
