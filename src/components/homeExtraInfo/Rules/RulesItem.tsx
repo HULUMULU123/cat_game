52c7aa788506e0d811bd2f852cc4f1d2acf40b76
Stas <stas.r.d87@gmail.com>
2025-08-23 15:07:18 +0700

    -

--- src/components/homeExtraInfo/Rules/RulesItem.tsx
+++ src/components/homeExtraInfo/Rules/RulesItem.tsx
@@ -6,13 +6,15 @@ display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
+gap: 7px;
 `
 
-const StyledRuleImg = styled.img``
+const StyledRuleImg = styled.img`
+width: 44px;`
 
 const StyledRuleText = styled.span`
 font-family: 'Roboto', sans-serif;
-font-size: 11px;
+font-size: 8px;
 text-align: center;
 color: var(--color-white-text);`
 
