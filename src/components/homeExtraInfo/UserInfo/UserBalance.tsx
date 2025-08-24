6e25e9504d2203c00a0c506ff280c1c6c9861239
Stas <stas.r.d87@gmail.com>
2025-08-24 21:07:53 +0700

    -

--- src/components/homeExtraInfo/UserInfo/UserBalance.tsx
+++ src/components/homeExtraInfo/UserInfo/UserBalance.tsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import styled from 'styled-components'
+import coin from '../../../assets/coin.png'
 
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  gap: 5px;
+  font-family:'Conthrax', sans-serif;
+`
+
+const StyledBlockHeader = styled.h3`
+  margin: 0;
+  padding:0;
+  display: inline;
+  color: var(--color-white-text);
+  font-size: 12px;
+  font-weight:500;
+`
+
+const StyledCoinWrapper = styled.div`
+  width: 100%;
+  background: #126358;
+  background: linear-gradient(216deg, rgba(18, 99, 88, 0.4) 50%, rgba(119, 162, 148, 0.2) 100%);
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  border-radius: 7px;
+`
+
+const StyledCoinContent = styled.div`
+  width: 70%;
+  display: flex;
+  align-items: center;
+`
+
+const StyledCoinImg = styled.img`
+width: 30px;
+height: 30px;`
+
+const StyledCoinBalance = styled.span`
+font-size: 30px;
+font-weight: 700;
+`
 export default function UserBalance() {
   return (
-    <div>UserBalance</div>
+    <StyledWrapper>
+      <StyledBlockHeader>БАЛАНС CRASH</StyledBlockHeader>
+      <StyledCoinWrapper>
+        <StyledCoinContent>
+          <StyledCoinImg src={coin}/>
+          <StyledCoinBalance>5 500</StyledCoinBalance>
+        </StyledCoinContent>
+      </StyledCoinWrapper>
+    </StyledWrapper>
   )
 }
--- src/components/homeExtraInfo/UserInfo/UserContent.tsx
+++ src/components/homeExtraInfo/UserInfo/UserContent.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import UserBalance from './UserBalance'
 import UserStatistics from './UserStatistics'
+import styled from 'styled-components'
 
+const StyledWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items:center;
+width: 95%;
+margin: 0 auto;
+gap: 20px;
+`
 export default function UserContent() {
   return (
-    <div>
+    <StyledWrapper>
         <UserBalance/>
         <UserStatistics/>
-    </div>
+    </StyledWrapper>
   )
 }
