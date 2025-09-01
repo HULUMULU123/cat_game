c6ef52be620a8b573adca7fb1fb0d18170df0c65
Stas <stas.r.d87@gmail.com>
2025-09-01 10:07:22 +1000

    -

--- src/pages/Failure.tsx
+++ src/pages/Failure.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import FailrueHeader from '../components/failure/header/FailrueHeader'
+
 
 export default function Failure() {
   return (
-    <div>Failure</div>
+    <div>
+      <FailrueHeader/>
+    </div>
   )
 }
