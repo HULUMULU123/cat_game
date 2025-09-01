aa69815c1d2662eb5def5f1e1e4d2b89c120c19f
Stas <stas.r.d87@gmail.com>
2025-09-01 18:47:02 +1000

    -

--- src/components/failure/droplets/Droplets.tsx
+++ src/components/failure/droplets/Droplets.tsx
@@ -8,8 +8,8 @@ import drop5 from '../../../assets/drops/drop4.svg';
 
 const Wrapper = styled.div`
   position: relative;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   overflow: hidden;
 `;
 
