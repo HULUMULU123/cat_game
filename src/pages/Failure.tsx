b5649603746655d2726f184e46a3be3a078c285f
Stas <stas.r.d87@gmail.com>
2025-09-02 20:35:45 +1000

    -

--- src/pages/Failure.tsx
+++ src/pages/Failure.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import FailrueHeader from '../components/failure/header/FailrueHeader'
 import Droplets from '../components/failure/droplets/Droplets'
+import FailureFooter from '../components/failure/footer/FailureFooter'
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -22,6 +23,16 @@ left: 0;
 width: 100%;
 z-index: 1;
 `
+const StyledFooterWrapper = styled.div`
+position: absolute;
+display: flex;
+flex-direction: column;
+align-items: center;
+bottom:0;
+left: 0;
+width: 100%;
+z-index: 1;
+`
 
 
 export default function Failure() {
@@ -31,6 +42,9 @@ export default function Failure() {
         <FailrueHeader/>
       </StyledHeaderWrapper>
       <Droplets/>
+      <StyledFooterWrapper>
+        <FailureFooter />
+      </StyledFooterWrapper>
     </StyledWrapper>
   )
 }
