a834dad083c33747dd524f3a5ca02f9167b7283e
Stas <stas.r.d87@gmail.com>
2025-12-26 14:56:19 +1000

    -

--- src/components/homeExtraInfo/PrizeModal/PrizeCard.tsx
+++ src/components/homeExtraInfo/PrizeModal/PrizeCard.tsx
@@ -30,7 +30,7 @@ const StyledPrizeWrapper = styled.div`
 const StyledPrizeImg = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
 `;
 
 const StyledEmptyPrize = styled.div`
